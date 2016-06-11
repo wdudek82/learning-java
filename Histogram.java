f2e2ec89d11b8e360109ee7b0c4c860d018b0048
Wojciech Dudek <wojtek.dudek@ims.fm>
2016-06-11 22:07:31 +0200

    First set of exercises

---
+++ Histogram.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+public class Histogram {
+
+    public static void main(String[] args) {
+        Scanner data = null;
+        ArrayList<Integer> count;
+        // ArrayList count;
+        Integer idx;
+
+        try {
+            data = new Scanner(new File("test.dat"));
+        }
+        catch (IOException e) {
+            System.out.println("Sorry but I was unable to open your data file.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        count = new ArrayList<Integer>(10);
+        // count = new ArrayList(10);
+        for (Integer i=0; i<10; i++) {
+            count.add(i,0);
+        }
+
+        while (data.hasNextInt()) {
+            idx = data.nextInt();
+            count.set(idx, count.get(idx)+1);
+        }
+
+        idx = 0;
+        for (Integer i : count) {
+            System.out.println(idx + " occurred " + i + " times.");
+            idx++;
+        }
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	public static void main(String[] args) {
+
+        for (Integer n = 20, i = 0; n < 30; n++, i += 2) {
+            System.out.println(n + " " + i);
+        }
+	}
+}
+
