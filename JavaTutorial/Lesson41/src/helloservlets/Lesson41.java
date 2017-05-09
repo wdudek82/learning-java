a73bc683ae8938745422682bb7a3f988620d07a8
Wojciech Dudek <wdudek82@gmail.com>
2017-05-10 00:06:31 +0200

    Java Video Tutorial #42: Servlets #2

--- JavaTutorial/Lesson41/src/helloservlets/Lesson41.java
+++ JavaTutorial/Lesson41/src/helloservlets/Lesson41.java
@@ -11,11 +11,24 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "Lesson41")
 public class Lesson41 extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("yourname");
+        String theLang = request.getParameter("Language");
+        int firstNum = Integer.parseInt(request.getParameter("fnumber"));
+        int secondNum = Integer.parseInt(request.getParameter("snumber"));
+        int sumOfNums = firstNum + secondNum;
 
+        response.setContentType("text/html");
+        PrintWriter output = response.getWriter();
+        output.println("<html><body><h3>Hello " + userName + "</h3>");
+        output.println("<p>" + firstNum + " + " + secondNum + " = " + sumOfNums);
+        output.println("Speaks the lang: " + theLang);
+        output.println("</body></html>");
     }
 }
